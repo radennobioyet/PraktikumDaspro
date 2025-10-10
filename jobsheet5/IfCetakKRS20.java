0db18688af50f8e92309c64d2238d2167317d0a7
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2025-10-10 13:34:29 +0700

    Percobaan 1

---
+++ jobsheet5/IfCetakKRS20.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class IfCetakKRS20 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS Siakad ---");
+        System.out.println("Apakah UKT Sudah Lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan Cetak KRS dan minta tanda tangan DPA");
+        }
+    }
+}
