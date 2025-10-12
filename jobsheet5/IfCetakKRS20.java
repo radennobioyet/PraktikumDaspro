90e6fc943d618f2f3bbcb89ebbaaab385fe9e391
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2025-10-12 10:55:11 +0700

    Tugas 1

--- jobsheet5/IfCetakKRS20.java
+++ jobsheet5/IfCetakKRS20.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class IfCetakKRS20 {
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("--- Cetak KRS Siakad ---");
-        System.out.println("Apakah UKT Sudah Lunas? (true/false): ");
+        System.out.println("Apakah UKT Sudah Lunas? (ya/tidak): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan Cetak KRS dan minta tanda tangan DPA");
-        }
+               // Tambahan:versiTernaryOperator
+        String hasil = uktLunas ? "Pembayaran UKT terverifikasi\n Silakan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu";
+        System.out.println("\n===Versi Ternary Operator===");
+        System.out.println(hasil);
     }
 }
